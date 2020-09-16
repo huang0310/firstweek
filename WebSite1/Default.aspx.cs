50bb0dfb5c4d468d317079cf6afacd2e827d3b92
huang0310 <3139667492@qq.com>
2020-09-16 16:21:19 +0800

    a

--- WebSite1/Default.aspx.cs
+++ WebSite1/Default.aspx.cs
@@ -7,8 +7,10 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
+    private bool hello;
+
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        Console.WriteLine(hello);
     }
 }
