7caac425fd80ae2782838e99103ffc4fcf716f61
huang0310 <3139667492@qq.com>
2020-09-16 15:15:52 +0800

    s

---
+++ WebSite1/App_Code/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAdapter
+{
+    class Program
+    {
+        public static object Strings { get; private set; }
+
+        static void Main(string[] args)
+        {
+            
+            using (SqlConnection conn = new SqlConnection(GetDatabaseConnection()))
+            {
+                conn.Open();
+
+                
+                InitialiseDatabase(conn);
+
+                
+                DataSet ds = new DataSet();
+
+                
+                SqlDataAdapter da = new SqlDataAdapter();
+
+               
+                da.SelectCommand = GenerateSelectCommand(conn);
+
+                da.Fill(ds, "Region");
+
+                
+                Console.WriteLine("Data selected via a stored procedure");
+
+                foreach (DataRow aRow in ds.Tables["Region"].Rows)
+                {
+                    Console.WriteLine("  {0,-3} {1}", aRow[0], aRow[1]);
+                }
+
+                conn.Close();
+            }
+        }
+
+        /// <summary>
+        /// Create a command that will select all region records
+        /// </summary>
+        /// <param name="conn">The database connection</param>
+        /// <returns>A SqlCommand</returns>
+        private static SqlCommand GenerateSelectCommand(SqlConnection conn)
+        {
+            SqlCommand aCommand = new SqlCommand("RegionSelect", conn);
+
+            aCommand.CommandType = CommandType.StoredProcedure;
+            aCommand.UpdatedRowSource = UpdateRowSource.None;
+
+            return aCommand;
+        }
+        /// <summary>
+        /// Add in the stored procs if we need to
+        /// </summary>
+        /// <param name="conn"></param>
+        private static void InitialiseDatabase(SqlConnection conn)
+        {
+            SqlCommand cmd = new SqlCommand(Strings.SelectProc, conn);
+            cmd.ExecuteNonQuery();
+        }
+
+        static string GetDatabaseConnection()
+        {
+            // If you are using SQL Express then use this connection string...
+            //return "server=.\\SQLEXPRESS;" +
+            //    "integrated security=SSPI;" +
+            //    "database=Northwind";
+
+            // And if using full SQL Server then this is most likely correct...
+            return "server=(local);" +
+                "integrated security=SSPI;" +
+                "database=Northwind";
+        }
+    }
+}
---
+++ WebSite1/App_Code/Class3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Class3 的摘要说明
+/// </summary>
+public class Class3
+{
+    public Class3()
+    {
+        //
+        // TODO: 在此处添加构造函数逻辑
+        //
+    }
+}
